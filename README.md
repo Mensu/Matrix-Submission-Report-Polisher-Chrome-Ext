e866d40924030d7188cfdea1103e5cc58b9c8c45
Mensu <yxshw55@qq.com>
2016-07-05 23:48:17 +0800

    attempt to fix a bug where the background would repeatedly set extension's icon and title on a removed tab

---
+++ README.md
@@ -0,0 +1,21 @@
+#### Please feel free to fork the repository, open new issues as well as make pull requests. Thanks in advance for all your participation!
+
+
+# Download
+
+[``zip``](https://github.com/Mensu/Matrix-Submission-Report-Polisher-Chrome-Ext/releases/download/v0.1-alpha/v0.1-alpha.zip)
+``13.4 MB``
+
+[``tar.gz``](https://github.com/Mensu/Matrix-Submission-Report-Polisher-Chrome-Ext/releases/download/v0.1-alpha/v0.1-alpha.tar.gz)
+``13.4 MB``
+
+# Install
+
+- go to [``chrome://extensions``](chrome://extensions), have ``Developer Mode`` checked and click ``load an unpacked directory``
+
+![1](http://7xrahq.com1.z0.glb.clouddn.com/chrome-ext-installation-1.png)
+
+- select the downloaded folder
+
+![2](http://7xrahq.com1.z0.glb.clouddn.com/chrome-ext-installation-2.png)
+
--- js/checkIsOnline.js
+++ js/checkIsOnline.js
@@ -2,17 +2,24 @@ chrome.webRequest.onCompleted.addListener(function(details) {
   if (details.tabId == -1) return;
   chrome.pageAction.show(details.tabId);
   setInterval(function() {
-    return httpRequest('https://eden.sysu.edu.cn:8000/app-angular/courses/exams/home/views/home.client.view.html', function(err) {
-      chrome.pageAction.setIcon({
-        "tabId": details.tabId,
-        "path": {
-          "19": './img/' + ((err) ? 'offline.png' : 'online.png'),
-          "38": './img/' + ((err) ? 'offline.png' : 'online.png')
+    chrome.tabs.query({}, function(tabArray) {
+      var tabId = details.tabId;
+      tabArray.forEach(function(oneTab, index, self) {
+        if (oneTab.id == tabId) {
+          return httpRequest('https://eden.sysu.edu.cn:8000/app-angular/courses/exams/home/views/home.client.view.html', function(err) {
+            chrome.pageAction.setIcon({
+              "tabId": tabId,
+              "path": {
+                "19": './img/' + ((err) ? 'offline.png' : 'online.png'),
+                "38": './img/' + ((err) ? 'offline.png' : 'online.png')
+              }
+            });
+            chrome.pageAction.setTitle({
+              "tabId": tabId,
+              "title": (err) ? 'disconnected to Matrix' : 'connected to Matrix'
+            });
+          });
         }
-      });
-      chrome.pageAction.setTitle({
-        "tabId": details.tabId,
-        "title": (err) ? 'disconnected to Matrix' : 'connected to Matrix'
       });
     });
   }, 5000);
